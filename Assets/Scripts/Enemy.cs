f137d8b3238baaf9b38b40d45ea8726f9d238cc2
Austin Hall <austin.hall1215@gmail.com>
2022-08-26 19:45:49 -0500

    Cleaned up Code for Enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,14 @@ public class Enemy : MonoBehaviour
 {   
 
 
-[SerializeField]
+    [SerializeField]
     private float _speed = 4.0f;
 
 
     // variables
     void Start()
     {
-         //Instantiate(this.gameObject, position, Quaternion.identity);  
+         
     }
     
   
@@ -23,32 +23,18 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {  SpawnEnemy();
-        //move Enemy down 4m per sec. 
-         transform.Translate (Vector3.down * _speed * Time.deltaTime);
-        //if bottom of screen 
-        // respawn at top
-
+    {  
+        transform.Translate (Vector3.down * _speed * Time.deltaTime);
+       
         if(transform.position.y < -5.4f)
         {   
             transform.position = new Vector3(Random.Range(-11.3f, 11.3f), 7.4f, 0);
-
-
         }
-        //respawn at top with new random x position.
-        // vertical screen limits -5.4 to 7.4 y-axis
-         
-       
-
     }
 
-    void SpawnEnemy()
-    {   
-        //variables
-                 
                 
 
        
 
-    }
+  
 }
